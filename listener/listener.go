172f9e186d02cb4b7f60126a1b2932c8808db7ca
bncrypted <bncrypted@gmail.com>
2021-05-17 20:03:06 +0100

    rearranged listener directory structure

--- listener/listener.go
+++ listener/listener.go
@@ -1,15 +1,25 @@
-package listener
+package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
 	"net"
 )
+
+func main() {
+	port := flag.String("p", "8081", "Port to listen on")
+	flag.Parse()
+
+	listen(*port)
+}
 
 func listen(port string) {
 	ln, err := net.Listen("tcp", ":"+port)
 	if err != nil {
 		log.Fatal("Error creating listener on port", port)
 	}
+	fmt.Println("Listening on port", port)
 	for {
 		conn, err := ln.Accept()
 		if err != nil {
