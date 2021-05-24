f32ad62394d354eb0f36daaa4783be9cfc6b29a7
bncrypted <bncrypted@gmail.com>
2021-05-24 19:17:24 +0100

    added basic leaflet.js map

--- dashboard/src/components/heatmap.js
+++ dashboard/src/components/heatmap.js
@@ -1,6 +1,14 @@
+import { MapContainer, TileLayer } from 'react-leaflet'
+import './heatmap.css';
+
 const Heatmap = () => {
     return (
-        <div>This is the heatmap</div>
+        <MapContainer className="heatmap" center={[0, 0]} zoom={2}>
+            <TileLayer
+                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+        </MapContainer>
     );
 };
 
