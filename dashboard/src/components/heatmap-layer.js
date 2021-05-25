b5791341f2ccc4bfb5c4b64044e22ab391a3cddc
bncrypted <bncrypted@gmail.com>
2021-05-25 19:27:37 +0100

    added heatmap layer to map

---
+++ dashboard/src/components/heatmap-layer.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { useMap } from 'react-leaflet'
+import L from 'leaflet'
+import 'leaflet.heat'
+
+const HeatmapLayer = () => {
+    
+    const map = useMap();
+    const heatmapConfig = {
+        minOpacity: 0,
+        maxZoom: 3,
+        radius: 20,
+        blur: 15,
+        gradient: {
+            0.4: 'blue',
+            0.6: 'lime',
+            0.8: 'yellow',
+            0.9: 'orange',
+            1.0: 'red',
+        },
+    };
+
+    React.useEffect(() => {
+        fetch('https://ba2gcmccu2.execute-api.eu-west-2.amazonaws.com/prod/v1/heatmap-data')
+            .then(res => res.json())
+            .then(rawData => rawData.heatmap_data_points)
+            .then(rawDataPoints => {
+
+                let sortedDataPoints = rawDataPoints
+                    .map(point => [point.lat, point.lon, point.count])
+                    .sort(function(a, b){ return a[2]-b[2] });
+
+                let flattenedCount = 1
+                let lastCount = sortedDataPoints[0][2]
+                for (let i = 0; i < sortedDataPoints.length; i++) {
+                    if (sortedDataPoints[i][2] > lastCount) {
+                        flattenedCount++
+                    }
+                    lastCount = sortedDataPoints[i][2]
+                    sortedDataPoints[i][2] = flattenedCount
+                }
+
+                const maxCount = sortedDataPoints[sortedDataPoints.length-1][2]
+                const normalisedDataPoints = sortedDataPoints
+                    .map(point => [point[0], point[1], point[2]/maxCount]);
+                
+                L.heatLayer(normalisedDataPoints, heatmapConfig).addTo(map);
+            })
+            .catch(console.log);
+    }, []);
+
+    return null
+}
+
+export default HeatmapLayer
--- dashboard/src/components/heatmap.js
+++ dashboard/src/components/heatmap.js
@@ -1,9 +1,12 @@
+import React from 'react';
 import { MapContainer, TileLayer } from 'react-leaflet'
+import HeatmapLayer from './heatmap-layer'
 import './heatmap.css';
 
 const Heatmap = () => {
     return (
         <MapContainer className="heatmap" center={[0, 0]} zoom={2}>
+            <HeatmapLayer />
             <TileLayer
                 attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
