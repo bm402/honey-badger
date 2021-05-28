5f650c7deee3ba449a33d96626f64946a22043aa
bncrypted <bncrypted@gmail.com>
2021-05-28 17:27:00 +0100

    added deployment on github pages

--- dashboard/src/components/navbar.js
+++ dashboard/src/components/navbar.js
@@ -4,14 +4,14 @@ import BSNav from 'react-bootstrap/Nav'
 const Navbar = () => {
     return (    
         <BSNavbar bg="light" expand="lg">
-            <BSNavbar.Brand href="/">Honey Badger</BSNavbar.Brand>
+            <BSNavbar.Brand href="/honey-badger/#/">Honey Badger</BSNavbar.Brand>
             <BSNavbar.Toggle aria-controls="basic-navbar-nav" />
             <BSNavbar.Collapse id="basic-navbar-nav">
                 <BSNav>
-                    <BSNav.Link href="/heatmap">Heatmap</BSNav.Link>
+                    <BSNav.Link href="/honey-badger/#/heatmap">Heatmap</BSNav.Link>
                 </BSNav>
                 <BSNav>
-                    <BSNav.Link href="/stats">Stats</BSNav.Link>
+                    <BSNav.Link href="/honey-badger/#/stats">Stats</BSNav.Link>
                 </BSNav>
             </BSNavbar.Collapse>
         </BSNavbar>
